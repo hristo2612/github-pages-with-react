4736aef31ada98744e5a3a2dfa74ad3855ba3efc
Hristo  Bogoev <hristob@192.168.1.6>
2018-05-13 18:44:04 +0300

    Updates

---
+++ static/DynamicPage.e300e06914ed3e0ca730.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{191:function(e,a,n){"use strict";Object.defineProperty(a,"__esModule",{value:!0});var t=d(n(0)),l=n(54),u=d(n(74));function d(e){return e&&e.__esModule?e:{default:e}}a.default=function(){return t.default.createElement(u.default,null,t.default.createElement(l.Header,{as:"h2"},"Dynamically Loaded Page"),t.default.createElement("p",null,"Page was loaded asynchronously"))}}}]);
+//# sourceMappingURL=DynamicPage.e300e06914ed3e0ca730.js.map
